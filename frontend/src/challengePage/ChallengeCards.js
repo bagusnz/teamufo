5ae9dcfad2d429c1f06bb95d9134f93c80b77bef
Minje.Lee <Minje.Lee@andrena.de>
2023-10-20 17:37:52 +0200

    add Challenge point

--- frontend/src/challengePage/ChallengeCards.js
+++ frontend/src/challengePage/ChallengeCards.js
@@ -4,9 +4,9 @@ import ChallengeCard from "./ChallengeCard";
 
 const ChallengeCards = () => {
     const challenges = [
-        { id: 1, title: "Challenge 1" },
-        { id: 2, title: "Challenge 2" },
-        { id: 3, title: "Challenge 3" }
+        { id: 1, title: "Challenge 1", point: 10},
+        { id: 2, title: "Challenge 2", point: 5 },
+        { id: 3, title: "Challenge 3", point: 15}
     ];
 
     return (
