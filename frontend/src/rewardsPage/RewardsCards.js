1da927eae61de049d88f6ad21654c5f0f6289f0e
Minje.Lee <Minje.Lee@andrena.de>
2023-10-21 00:10:36 +0200

    add rewards cards

--- frontend/src/rewardsPage/RewardsCards.js
+++ frontend/src/rewardsPage/RewardsCards.js
@@ -1,5 +1,18 @@
+import React, { useState } from "react";
+import { rewards } from "../mockDatei/rewards";
+import RewardCard from "./RewardCard";
+import { users } from "../mockDatei/users";
+
 const RewardsCards = () => {
-  return <div>comming soon!</div>;
+  const [points, setPoints] = useState(users[0].points);
+  return (
+    <>
+      <h3>My Points: {points}</h3>
+      {rewards.map((reward) => (
+        <RewardCard key={reward.id} reward={reward} />
+      ))}
+    </>
+  );
 };
 
 export default RewardsCards;
