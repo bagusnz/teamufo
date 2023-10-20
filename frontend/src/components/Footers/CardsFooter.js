e01aab4baf5fc6c08a220cef5d01b7c30413dea4
hyanggi <fragrance0627@gmail.com>
2023-10-20 15:31:56 +0200

    remove footer fotos

--- frontend/src/components/Footers/CardsFooter.js
+++ frontend/src/components/Footers/CardsFooter.js
@@ -37,30 +37,6 @@ class CardsFooter extends React.Component {
     return (
       <>
         <footer className="footer has-cards">
-          <Container className="container-lg">
-            <Row>
-              <Col className="mb-5 mb-md-0" md="6">
-                <Card className="card-lift--hover shadow border-0">
-                  <Link to="/landing-page">
-                    <CardImg
-                      alt="..."
-                      src={require("assets/img/theme/landing.jpg")}
-                    />
-                  </Link>
-                </Card>
-              </Col>
-              <Col className="mb-5 mb-lg-0" md="6">
-                <Card className="card-lift--hover shadow border-0">
-                  <Link to="/profile-page">
-                    <CardImg
-                      alt="..."
-                      src={require("assets/img/theme/profile.jpg")}
-                    />
-                  </Link>
-                </Card>
-              </Col>
-            </Row>
-          </Container>
           <Container>
             <Row className="row-grid align-items-center my-md">
               <Col lg="6">
