a217bf555b37b18c48e2de53ff8d6e87576128f6
Hyang Gi <fragrance0627@gmail.com>
2023-10-21 05:28:34 +0200

    modified the reward site

--- frontend/src/rewardTemplate/RewardTemplate.js
+++ frontend/src/rewardTemplate/RewardTemplate.js
@@ -35,89 +35,6 @@ const RewardTemplate = () => {
                 </div>
             </div>
 
-            <div>
-                <div className="container mt-5 mb-3">
-                    <div className="row">
-                        <div className="col-md-4">
-                            <div className="card p-3 mb-2">
-                                <div className="d-flex justify-content-between">
-                                    <div className="d-flex flex-row align-items-center">
-                                        <div className="icon"><i className="bx bxl-mailchimp"></i></div>
-                                        <div className="ms-2 c-details">
-                                            <h6 className="mb-0">Mailchimp</h6> <span>1 days ago</span>
-                                        </div>
-                                    </div>
-                                    <div className="badge"><span>Design</span></div>
-                                </div>
-                                <div className="mt-5">
-                                    <h3 className="heading">
-                                        Boucher - <br />Regional Product
-                                    </h3>
-                                    <div className="mt-5">
-                                        <div className="progress">
-                                            <div className="progress-bar" role="progressbar" style={{width: 50}}
-                                                 aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                                        </div>
-                                        <div className="mt-3"><span className="text1">32 Applied <span
-                                            className="text2">of 50 capacity</span></span></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="card p-3 mb-2">
-                                <div className="d-flex justify-content-between">
-                                    <div className="d-flex flex-row align-items-center">
-                                        <div className="icon"><i className="bx bxl-dribbble"></i></div>
-                                        <div className="ms-2 c-details">
-                                            <h6 className="mb-0">Dribbble</h6> <span>4 days ago</span>
-                                        </div>
-                                    </div>
-                                    <div className="badge"><span>Product</span></div>
-                                </div>
-                                <div className="mt-5">
-                                    <h3 className="heading">Junior Product
-                                        Designer-Singapore</h3>
-                                    <div className="mt-5">
-                                        <div className="progress">
-                                            <div className="progress-bar" role="progressbar" style={{width: 50}}
-                                                 aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                                        </div>
-                                        <div className="mt-3"><span className="text1">42 Applied <span
-                                            className="text2">of 70 capacity</span></span></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="card p-3 mb-2">
-                                <div className="d-flex justify-content-between">
-                                    <div className="d-flex flex-row align-items-center">
-                                        <div className="icon"><i className="bx bxl-reddit"></i></div>
-                                        <div className="ms-2 c-details">
-                                            <h6 className="mb-0">Reddit</h6> <span>2 days ago</span>
-                                        </div>
-                                    </div>
-                                    <div className="badge"><span>Design</span></div>
-                                </div>
-                                <div className="mt-5">
-                                    <h3 className="heading">Software Architect
-                                        Java - USA</h3>
-                                    <div className="mt-5">
-                                        <div className="progress">
-                                            <div className="progress-bar" role="progressbar" style={{width: 50}}
-                                                 aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                                        </div>
-                                        <div className="mt-3"><span className="text1">52 Applied <span
-                                            className="text2">of 100 capacity</span></span></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
 
             <div>
                 <Container className="mt-5 mb-3">
@@ -130,18 +47,21 @@ const RewardTemplate = () => {
                                             <i className="bx bxl-mailchimp"></i>
                                         </div>
                                         <div className="ms-2 c-details">
-                                            <h6 className="mb-0">Mailchimp</h6>
+                                            <h6 className="mb-0">Just For You</h6>
                                             <span>1 day ago</span>
                                         </div>
                                     </div>
                                     <div className="badge">
-                                        <span>Design</span>
+                                        <Button variant="danger">- 1000 P</Button>
                                     </div>
                                 </div>
                                 <div className="mt-5">
                                     <h3 className="heading">
                                         Boucher - <br />Regional Product
                                     </h3>
+                                    <div className="c-details">
+                                        <p className="mb-0">You can get a rebate with this boucher.</p>
+                                    </div>
                                     <div className="mt-5">
                                         <ProgressBar variant="success"  now={50} />
                                         <div className="mt-3">
@@ -161,16 +81,21 @@ const RewardTemplate = () => {
                                             <i className="bx bxl-dribbble"></i>
                                         </div>
                                         <div className="ms-2 c-details">
-                                            <h6 className="mb-0">Dribbble</h6>
+                                            <h6 className="mb-0">Tree matters</h6>
                                             <span>4 days ago</span>
                                         </div>
                                     </div>
                                     <div className="badge">
-                                        <span>Product</span>
+                                        <Button variant="warning">- 1000 P</Button>
                                     </div>
                                 </div>
                                 <div className="mt-5">
                                     <h3 className="heading">Donate - <br />Planting Project</h3>
+
+                                    <div className="c-details">
+                                        <p className="mb-0">Let's plant a tree together.</p>
+                                    </div>
+
                                     <div className="mt-5">
                                         <ProgressBar variant="info" now={50} />
                                         <div className="mt-3">
@@ -190,16 +115,20 @@ const RewardTemplate = () => {
                                             <i className="bx bxl-reddit"></i>
                                         </div>
                                         <div className="ms-2 c-details">
-                                            <h6 className="mb-0">Reddit</h6>
+                                            <h6 className="mb-0">Hessen Tomaten 🍅</h6>
                                             <span>2 days ago</span>
                                         </div>
                                     </div>
                                     <div className="badge">
-                                        <span>Design</span>
+                                        <Button variant="success">- 1000 P</Button>
                                     </div>
                                 </div>
                                 <div className="mt-5">
-                                    <h3 className="heading">Exchange - <br/>Regional Product <br/>Hessen Tomaten 🍅</h3>
+                                    <h3 className="heading">Exchange - <br/>Eine Tomate 🍅</h3>
+
+                                    <div className="c-details">
+                                        <p className="mb-0">Delicious Tomato for you</p>
+                                    </div>
                                     <div className="mt-5">
                                         <ProgressBar variant="warning" now={50} />
                                         <div className="mt-3">
