0d51ef94ca02eec322715828999679120cf4fe2f
Hyang Gi <fragrance0627@gmail.com>
2023-10-21 05:19:24 +0200

    Modify Reward design

--- frontend/src/rewardTemplate/RewardTemplate.js
+++ frontend/src/rewardTemplate/RewardTemplate.js
@@ -7,6 +7,7 @@ import bgimg from '../img/v990-129a.jpg';
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Badge from "react-bootstrap/Badge";
+import ProgressBar from "react-bootstrap/ProgressBar";
 const trans = `rgba(210, 210, 210, 0.9)`;
 
 const RewardTemplate = () => {
@@ -33,6 +34,7 @@ const RewardTemplate = () => {
                         Get Your Rewards</h1></p>
                 </div>
             </div>
+
             <div>
                 <div className="container mt-5 mb-3">
                     <div className="row">
@@ -48,8 +50,9 @@ const RewardTemplate = () => {
                                     <div className="badge"><span>Design</span></div>
                                 </div>
                                 <div className="mt-5">
-                                    <h3 className="heading">Senior Product
-                                        Designer-Singapore</h3>
+                                    <h3 className="heading">
+                                        Boucher - <br />Regional Product
+                                    </h3>
                                     <div className="mt-5">
                                         <div className="progress">
                                             <div className="progress-bar" role="progressbar" style={{width: 50}}
@@ -114,6 +117,102 @@ const RewardTemplate = () => {
                     </div>
                 </div>
 
+            </div>
+
+            <div>
+                <Container className="mt-5 mb-3">
+                    <Row>
+                        <Col md={4}>
+                            <Card className="p-3 mb-2">
+                                <div className="d-flex justify-content-between">
+                                    <div className="d-flex flex-row align-items-center">
+                                        <div className="icon">
+                                            <i className="bx bxl-mailchimp"></i>
+                                        </div>
+                                        <div className="ms-2 c-details">
+                                            <h6 className="mb-0">Mailchimp</h6>
+                                            <span>1 day ago</span>
+                                        </div>
+                                    </div>
+                                    <div className="badge">
+                                        <span>Design</span>
+                                    </div>
+                                </div>
+                                <div className="mt-5">
+                                    <h3 className="heading">
+                                        Boucher - <br />Regional Product
+                                    </h3>
+                                    <div className="mt-5">
+                                        <ProgressBar variant="success"  now={50} />
+                                        <div className="mt-3">
+                    <span className="text1">
+                      32 Applied <span className="text2">of 50 capacity</span>
+                    </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Card>
+                        </Col>
+                        <Col md={4}>
+                            <Card className="p-3 mb-2">
+                                <div className="d-flex justify-content-between">
+                                    <div className="d-flex flex-row align-items-center">
+                                        <div className="icon">
+                                            <i className="bx bxl-dribbble"></i>
+                                        </div>
+                                        <div className="ms-2 c-details">
+                                            <h6 className="mb-0">Dribbble</h6>
+                                            <span>4 days ago</span>
+                                        </div>
+                                    </div>
+                                    <div className="badge">
+                                        <span>Product</span>
+                                    </div>
+                                </div>
+                                <div className="mt-5">
+                                    <h3 className="heading">Donate - <br />Planting Project</h3>
+                                    <div className="mt-5">
+                                        <ProgressBar variant="info" now={50} />
+                                        <div className="mt-3">
+                    <span className="text1">
+                      42 Applied <span className="text2">of 70 capacity</span>
+                    </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Card>
+                        </Col>
+                        <Col md={4}>
+                            <Card className="p-3 mb-2">
+                                <div className="d-flex justify-content-between">
+                                    <div className="d-flex flex-row align-items-center">
+                                        <div className="icon">
+                                            <i className="bx bxl-reddit"></i>
+                                        </div>
+                                        <div className="ms-2 c-details">
+                                            <h6 className="mb-0">Reddit</h6>
+                                            <span>2 days ago</span>
+                                        </div>
+                                    </div>
+                                    <div className="badge">
+                                        <span>Design</span>
+                                    </div>
+                                </div>
+                                <div className="mt-5">
+                                    <h3 className="heading">Exchange - <br/>Regional Product <br/>Hessen Tomaten 🍅</h3>
+                                    <div className="mt-5">
+                                        <ProgressBar variant="warning" now={50} />
+                                        <div className="mt-3">
+                    <span className="text1">
+                      52 Applied <span className="text2">of 100 capacity</span>
+                    </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         </>
     );
